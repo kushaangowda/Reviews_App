1db2d0c25f8dc324a5cb5c4d33064c8c593b2327
Kushaan Gowda <kushaangowda@yahoo.com>
2020-11-25 13:35:04 +0530

    added files

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,57 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+	View,
+	Text,
+	FlatList,
+	TouchableOpacity,
+	Modal,
+	StyleSheet,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from "react-native";
 import Card from "../shared/Card";
 import { globalStyles } from "../styles/Global";
+import { MaterialIcons } from "@expo/vector-icons";
+import ReviewForm from "./ReviewForm";
 
 export default function Home({ navigation }) {
+	const [modalOpen, setModalOpen] = useState(false);
+
 	const [reviews, setReviews] = useState([
 		{ title: "Movie 1", rating: 5, body: "lorem ipsum", key: "1" },
 		{ title: "Movie 2", rating: 4, body: "lorem ipsum", key: "2" },
 		{ title: "Movie 3", rating: 2, body: "lorem ipsum", key: "3" },
 	]);
 
+	const addReview = (review) => {
+		review.key = Math.random().toString() + Math.random().toString();
+		setReviews((currentReviews) => {
+			return [review, ...currentReviews];
+		});
+		setModalOpen(false);
+	};
+
 	return (
 		<View style={globalStyles.container}>
+			<Modal visible={modalOpen} animationType="slide">
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<View style={styles.modalContent}>
+						<MaterialIcons
+							name="close"
+							size={24}
+							onPress={() => setModalOpen(false)}
+							style={{ ...styles.modalToggle, ...styles.modalClose }}
+						/>
+						<ReviewForm addReview={addReview} />
+					</View>
+				</TouchableWithoutFeedback>
+			</Modal>
+			<MaterialIcons
+				name="add"
+				size={24}
+				onPress={() => setModalOpen(true)}
+				style={styles.modalToggle}
+			/>
 			<FlatList
 				data={reviews}
 				renderItem={({ item }) => {
@@ -31,3 +71,21 @@ export default function Home({ navigation }) {
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	modalToggle: {
+		marginBottom: 10,
+		borderWidth: 1,
+		borderColor: "#aaa",
+		borderRadius: 10,
+		alignSelf: "center",
+		padding: 10,
+	},
+	modalClose: {
+		marginTop: 20,
+		marginBottom: 0,
+	},
+	modalContent: {
+		flex: 1,
+	},
+});
--- styles/Global.js
+++ styles/Global.js
@@ -15,6 +15,22 @@ export const globalStyles = StyleSheet.create({
 		marginVertical: 8,
 		lineHeight: 20,
 	},
+	input: {
+		borderWidth: 1,
+		borderColor: "#ddd",
+		padding: 10,
+		fontSize: 18,
+		borderRadius: 6,
+		marginTop: 5,
+		marginBottom: 10,
+	},
+	errorText: {
+		color: "crimson",
+		fontWeight: "bold",
+		marginBottom: 10,
+		marginTop: 6,
+		textAlign: "center",
+	},
 });
 
 export const images = {
